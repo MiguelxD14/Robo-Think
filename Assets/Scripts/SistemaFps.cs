33ee534a591c0b3a1a06dc65c5d7354fcfefd93a
MiguelxD14 <miguelbezerra135@gmail.com>
2022-05-24 18:15:50 -0300

    Sistema FPS feito
    Funciona da seguinte maneira: Clicando "E" você pega o bloco, e clicando "Q" você solta o bloco.

--- Assets/Scripts/SistemaFps.cs
+++ Assets/Scripts/SistemaFps.cs
@@ -5,26 +5,78 @@ using UnityEngine;
 public class SistemaFps : MonoBehaviour
 {
     public Transform mao;
-    public GameObject bloco;
+    public float distance = 10f;
+    GameObject blocoAtual;
+    GameObject bloco;
 
-    void OnMouseDown()
+    bool canGrab;
+
+    private void Update()
     {
-        if (Input.GetMouseButton(0))
-        {
-            GetComponent<Rigidbody>().isKinematic = true;
-            bloco.transform.position = mao.transform.position;
-            bloco.transform.rotation = mao.transform.rotation;
-            bloco.transform.SetParent(mao);
-        }
-    }
+        CheckGrab();
 
-    void OnMouseUp()
+        if (canGrab)
         {
-            bloco.transform.parent = null;
-            GetComponent<Rigidbody>().isKinematic = false;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (blocoAtual != null)
+                     Soltar();
+
+                Pegar();
+            }
         }
 
+        if (blocoAtual != null)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                Soltar();
+
+            }
+        }
+
     }
+        private void CheckGrab()
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, distance))
+            {
+                if (hit.transform.tag == "canGrab")
+                {
+                    Debug.Log("Pegue o bloco");
+                    canGrab = true;
+                    bloco = hit.transform.gameObject;
+                }
+            }
+            else
+            {
+                canGrab = false;
+
+            }
+        }
+ 
+    private void Pegar()
+    {
+        blocoAtual = bloco;
+        blocoAtual.transform.position = mao.position;
+        blocoAtual.transform.parent = mao;
+        blocoAtual.transform.localEulerAngles = new Vector3(0, 180, 0);
+        blocoAtual.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    private void Soltar()
+    {
+        blocoAtual = bloco;
+        blocoAtual.transform.parent = null;
+        blocoAtual.GetComponent<Rigidbody>().isKinematic = false;
+        blocoAtual = null;
+
+    }
+}
+    
+
+    
 
 
 
