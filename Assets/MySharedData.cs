0a9ad5071c3f8ef6271299ac928d5edc7e083ea6
MiguelxD14 <101884554+MiguelxD14@users.noreply.github.com>
2022-09-09 21:53:19 -0300

    Codigo de coleta

--- Assets/MySharedData.cs
+++ Assets/MySharedData.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class MySharedData : MonoBehaviour
 {
+    string filename = "";
+
+    [System.Serializable]
+    public class dados
+    {
+        public string fase, resultado;
+        public string Data_hora;
+
+    }
+
+    [System.Serializable]
+    public class ListaFases
+    {
+        public dados[] Cenas;
+    }
+
+    public ListaFases myListaFases = new ListaFases();
     // Start is called before the first frame update
     void Start()
     {
-        
+        filename = Application.dataPath + "/coletateste.csv";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (PlayerPrefs.GetString("acabou") == "true")
+        {
+            WriteCSV();
+        }
+    }
+
+
+    public void WriteCSV()
+    {
+        if (myListaFases.Cenas.Length > 0)
+        {
+            TextWriter tw = new StreamWriter(filename, false);
+            tw.WriteLine("Data/Hora, Fase, Resultado ");
+            tw.Close();
+
+            tw = new StreamWriter(filename, true);
+
+            for (int i = 0; i < myListaFases.Cenas.Length; i++)
+            {
+                myListaFases.Cenas[i].Data_hora = PlayerPrefs.GetString("Data");
+                myListaFases.Cenas[i].fase = PlayerPrefs.GetString("Fase");
+                myListaFases.Cenas[i].resultado = PlayerPrefs.GetString("Concluiu");
+
+                tw.WriteLine(myListaFases.Cenas[i].Data_hora + "," +
+                    myListaFases.Cenas[i].fase + "," + myListaFases.Cenas[i].resultado);
+            }
+
+            tw.Close();
+        }
+        PlayerPrefs.SetString("acabou", "false");
+
     }
 }
