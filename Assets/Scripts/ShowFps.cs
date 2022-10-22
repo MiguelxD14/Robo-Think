7dbbe73f15ce897e505d393e33909d0e5b447e20
MiguelxD14 <miguelbezerra135@gmail.com>
2022-10-21 21:38:53 -0300

    Analytics + Otimzações - part3

--- Assets/Scripts/ShowFps.cs
+++ Assets/Scripts/ShowFps.cs
@@ -8,7 +8,7 @@ public class ShowFps : MonoBehaviour
     public Text fpsText;
     public float deltaTime;
  
-     void Update () 
+     void Update() 
      {
          deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
          float fps = 1.0f / deltaTime;
